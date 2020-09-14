a20af037838e930ebbe7fd1f20e6464623ddc096
mkashyap69 <47946021+mkashyap69@users.noreply.github.com>
2020-09-14 10:25:44 +0530

    adding styles to homepage and dividing it into components

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,36 +1,9 @@
 import React from 'react';
+import Directory from './Directory';
 import './styles/homepage.styles.scss';
 
 export const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">PANTS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SHIRTS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">T-SHIRTS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory/>
   </div>
 );
