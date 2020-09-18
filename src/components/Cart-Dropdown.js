e3a5e032552cebfb4b1b2dde669c4e6091063409
mkashyap69 <47946021+mkashyap69@users.noreply.github.com>
2020-09-18 10:50:41 +0530

    cart item component and cart dropdown completed

--- src/components/Cart-Dropdown.js
+++ src/components/Cart-Dropdown.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import CartItem from './CartItem';
 import CustomButton from './CustomButton';
 
 import './styles/cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cart_items }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cart_items.map((cart_item) => (
+        <CartItem key={cart_item.id} item={cart_item} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cart_items } }) => ({
+  //we are destructuring cart out of state and then carItems out of cart
+  cart_items,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
